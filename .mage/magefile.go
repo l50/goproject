bfef1692520d4ad8c45d3472cd20c1d44cb791dc
Jayson Grace <jayson.e.grace@gmail.com>
2022-07-01 00:19:52 -0600

    Initial commit

---
+++ .mage/magefile.go
@@ -0,0 +1,111 @@
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fatih/color"
+	utils "github.com/l50/goutils"
+
+	// mage utility functions
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+func init() {
+	os.Setenv("GO111MODULE", "on")
+}
+
+// InstallDeps Installs go dependencies
+func InstallDeps() error {
+	fmt.Println(color.YellowString("Installing dependencies."))
+
+	if err := utils.Tidy(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install dependencies: %v", err))
+	}
+
+	if err := utils.InstallGoPCDeps(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install pre-commit dependencies: %v", err))
+	}
+
+	if err := utils.InstallVSCodeModules(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install vscode-go modules: %v", err))
+	}
+
+	return nil
+}
+
+// InstallPreCommitHooks Installs pre-commit hooks locally
+func InstallPreCommitHooks() error {
+	mg.Deps(InstallDeps)
+
+	fmt.Println(color.YellowString("Installing pre-commit hooks."))
+	if err := utils.InstallPCHooks(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RunPreCommit runs all pre-commit hooks locally
+func RunPreCommit() error {
+	mg.Deps(InstallDeps)
+
+	fmt.Println(color.YellowString("Updating pre-commit hooks."))
+	if err := utils.UpdatePCHooks(); err != nil {
+		return err
+	}
+
+	fmt.Println(color.YellowString(
+		"Clearing the pre-commit cache to ensure we have a fresh start."))
+	if err := utils.ClearPCCache(); err != nil {
+		return err
+	}
+
+	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
+	if err := sh.RunV("pre-commit", "run", "--all-files"); err != nil {
+		return fmt.Errorf(color.RedString("failed to run pre-commit hooks: %v", err))
+	}
+
+	return nil
+}
+
+// RunTests runs all of the unit tests
+func RunTests() error {
+	mg.Deps(InstallDeps)
+
+	fmt.Println(color.YellowString("Running unit tests."))
+	if err := sh.RunV(filepath.Join(".hooks", "go-unit-tests.sh")); err != nil {
+		return fmt.Errorf(color.RedString("failed to run unit tests: %v", err))
+	}
+
+	return nil
+}
+
+// UpdateMirror updates pkg.go.dev with the release associated with the input tag
+func UpdateMirror(tag string) error {
+	var err error
+	fmt.Println(color.YellowString("Updating pkg.go.dev with the new tag %s.", tag))
+
+	err = sh.RunV("curl", "--silent", fmt.Sprintf(
+		"https://sum.golang.org/lookup/github.com/l50/goproject@%s",
+		tag))
+	if err != nil {
+		return fmt.Errorf(color.RedString("failed to update proxy.golang.org: %w", err))
+	}
+
+	err = sh.RunV("curl", "--silent", fmt.Sprintf(
+		"https://proxy.golang.org/github.com/l50/goproject/@v/%s.info",
+		tag))
+	if err != nil {
+		return fmt.Errorf(color.RedString("failed to update pkg.go.dev: %w", err))
+	}
+
+	return nil
+}
