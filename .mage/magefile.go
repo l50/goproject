48c09ef4d556a69d90d9750f2ffdf54035ac0492
Jayson Grace <jayson.e.grace@gmail.com>
2022-07-23 16:08:06 -0600

    Update pc run all; vscode files work for mage debugging

--- .mage/magefile.go
+++ .mage/magefile.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/bitfield/script"
 	"github.com/fatih/color"
 	utils "github.com/l50/goutils"
 
@@ -69,7 +70,11 @@ func RunPreCommit() error {
 	}
 
 	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
-	if err := sh.RunV("pre-commit", "run", "--all-files"); err != nil {
+	cmd := "pre-commit run --show-diff-on-failure --color=always --all-files"
+
+	_, err := script.Exec(cmd).String()
+
+	if err != nil {
 		return fmt.Errorf(color.RedString("failed to run pre-commit hooks: %v", err))
 	}
 
