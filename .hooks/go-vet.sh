085b0a9552c5de3d899cadb9b08a49b51e11f15d
Jayson Grace <jayson.e.grace@gmail.com>
2022-10-10 10:16:12 -0600

    Remove old hook; add magefiles to go-vet exclusions

--- .hooks/go-vet.sh
+++ .hooks/go-vet.sh
@@ -6,6 +6,7 @@ for dir in */; do
     if [[ "${dir}" != ".mage" ]] \
                               && [[ "${dir}" != "config/" ]] \
                               && [[ "${dir}" != "bin/" ]] \
+                              && [[ "${dir}" != "magefiles/" ]] \
                            && [[ "${dir}" != "resources/" ]] \
                                  && [[ "${dir}" != "docs/" ]] \
                             && [[ "${dir}" != "files/" ]] \
