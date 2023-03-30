e70d85a9fc697519c96dee0d1c73a1835fdd32ef
Jayson Grace <jayson.e.grace@gmail.com>
2023-03-30 19:03:18 +0000

    Update several linting and PC components

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -82,7 +81,7 @@ func RunTests() error {
 	mg.Deps(InstallDeps)
 
 	fmt.Println(color.YellowString("Running unit tests."))
-	if err := sh.RunV(filepath.Join(".hooks", "go-unit-tests.sh")); err != nil {
+	if err := sh.RunV(filepath.Join(".hooks", "go-unit-tests.sh"), "all"); err != nil {
 		return fmt.Errorf(color.RedString("failed to run unit tests: %v", err))
 	}
 
@@ -106,58 +105,6 @@ func UpdateMirror(tag string) error {
 		tag))
 	if err != nil {
 		return fmt.Errorf(color.RedString("failed to update pkg.go.dev: %w", err))
-	}
-
-	return nil
-}
-
-// Compile Generates a binary (for the input OS if `osCli` is set)
-// or set of binaries associated with the project.
-//
-// Examples:
-//
-// ```bash
-// # macOS
-// ./magefile compile darwin
-// # windows, linux, darwin
-// ./magefile compile all
-// ```
-func Compile(ctx context.Context, osCli string, binName string) error {
-	var operatingSystems []string
-	binDir := "."
-
-	if osCli == "all" {
-		operatingSystems = []string{"windows", "linux", "darwin"}
-	} else {
-		operatingSystems = []string{osCli}
-	}
-
-	if binName == "" {
-		return fmt.Errorf(color.RedString(
-			"failed to input binName! The current value is %s. "+
-				"Try again using this format: ./magefile compile darwin myBin", binName))
-	}
-
-	// Create bin/ if it doesn't already exist
-	if _, err := os.Stat(binDir); os.IsNotExist(err) {
-		if err := os.Mkdir(binDir, os.ModePerm); err != nil {
-			return fmt.Errorf(color.RedString(
-				"failed to create bin dir: %v", err))
-		}
-	}
-
-	for _, os := range operatingSystems {
-		fmt.Printf(color.YellowString("Compiling %s bin for %s OS, please wait.\n", binName, os))
-		env := map[string]string{
-			"GOOS":   os,
-			"GOARCH": "amd64",
-		}
-
-		binPath := filepath.Join(binDir, fmt.Sprintf("%s-%s", binName, os))
-
-		if err := sh.RunWith(env, "go", "build", "-o", binPath); err != nil {
-			return fmt.Errorf(color.RedString("failed to create %s bin: %v", binPath, err))
-		}
 	}
 
 	return nil
